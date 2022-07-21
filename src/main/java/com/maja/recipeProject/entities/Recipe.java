dee45693e03fcf5383215f74e2487ab1fee28d20
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-21 22:52:28 +0200

    Added Entities Recipe and Notes. Closes #1
    @OneToOne

--- src/main/java/com/maja/recipeProject/entities/Recipe.java
+++ src/main/java/com/maja/recipeProject/entities/Recipe.java
@@ -1,32 +1,104 @@
 package com.maja.recipeProject.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Recipe {
 
-    public Long id;
-    public String name;
-    public String type;
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String description;
+   private Integer prepTime;
+   private Integer cookTime;
+   private Integer servings;
+   private String source;
+   private String url;
+   private String directions;
+   //todo add
+    //private Difficulty difficulty
+   @Lob
+   private Byte[] image;
+   @OneToOne(cascade = CascadeType.ALL)
+   private Notes notes;
 
-    public String getName() {
-        return name;
-    }
+   public Long getId() {
+      return id;
+   }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+   public void setId(Long id) {
+      this.id = id;
+   }
 
-    public String getType() {
-        return type;
-    }
+   public String getDescription() {
+      return description;
+   }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+   public void setDescription(String description) {
+      this.description = description;
+   }
 
-    public Long getId() {
-        return id;
-    }
+   public Integer getPrepTime() {
+      return prepTime;
+   }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+   public void setPrepTime(Integer prepTime) {
+      this.prepTime = prepTime;
+   }
+
+   public Integer getCookTime() {
+      return cookTime;
+   }
+
+   public void setCookTime(Integer cookTime) {
+      this.cookTime = cookTime;
+   }
+
+   public Integer getServings() {
+      return servings;
+   }
+
+   public void setServings(Integer servings) {
+      this.servings = servings;
+   }
+
+   public String getSource() {
+      return source;
+   }
+
+   public void setSource(String source) {
+      this.source = source;
+   }
+
+   public String getUrl() {
+      return url;
+   }
+
+   public void setUrl(String url) {
+      this.url = url;
+   }
+
+   public String getDirections() {
+      return directions;
+   }
+
+   public void setDirections(String directions) {
+      this.directions = directions;
+   }
+
+   public Byte[] getImage() {
+      return image;
+   }
+
+   public void setImage(Byte[] image) {
+      this.image = image;
+   }
+
+   public Notes getNotes() {
+      return notes;
+   }
+
+   public void setNotes(Notes notes) {
+      this.notes = notes;
+   }
 }
