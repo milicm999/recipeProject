1544bcc88b8f96ba50748b9a76fe670e7b2fe70e
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-21 23:21:51 +0200

    Added entity Ingredient. Closes #2

--- src/main/java/com/maja/recipeProject/entities/Recipe.java
+++ src/main/java/com/maja/recipeProject/entities/Recipe.java
@@ -1,6 +1,7 @@
 package com.maja.recipeProject.entities;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -17,10 +18,20 @@ public class Recipe {
    private String directions;
    //todo add
     //private Difficulty difficulty
+   @OneToMany(cascade = CascadeType.ALL , mappedBy = "recipe")
+   private Set<Ingredient> ingredients;
    @Lob
    private Byte[] image;
    @OneToOne(cascade = CascadeType.ALL)
    private Notes notes;
+
+   public Set<Ingredient> getIngredients() {
+      return ingredients;
+   }
+
+   public void setIngredients(Set<Ingredient> ingredients) {
+      this.ingredients = ingredients;
+   }
 
    public Long getId() {
       return id;
