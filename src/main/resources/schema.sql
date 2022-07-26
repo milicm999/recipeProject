60523daa9d0319bbedf44bb3248685da818ab004
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-26 12:27:22 +0200

    Adding bootstrap data on startup. Closes #7

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+INSERT INTO CATEGORY (description) VALUES ('American');
+INSERT INTO CATEGORY (description) VALUES ('Italian');
+INSERT INTO CATEGORY (description) VALUES ('Mexican');
+INSERT INTO CATEGORY (description) VALUES ('Fast Food');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Teaspoon');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Tablespoon');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Cup');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Pinch');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Ounce');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Ripe');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Dash');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Each');
+INSERT INTO UNIT_OF_MEASURE (description) VALUES ('Pounds');
