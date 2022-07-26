fcb072e979ed5a068d0dc72135574ce9b8308a4c
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-26 12:30:41 +0200

    Displaying list of Recipes. Closes #9

--- src/main/java/com/maja/recipeProject/controllers/RecipeController.java
+++ src/main/java/com/maja/recipeProject/controllers/RecipeController.java
@@ -1,17 +1,29 @@
 package com.maja.recipeProject.controllers;
 
+import com.maja.recipeProject.entities.Category;
+import com.maja.recipeProject.entities.UnitOfMeasure;
+import com.maja.recipeProject.repositories.CategoryRepository;
+import com.maja.recipeProject.repositories.UnitOfMeasureRepository;
 import com.maja.recipeProject.services.RecipeService;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class RecipeController {
 
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @RequestMapping({"/recipes","/recipes/index.html","/recipes/index"})
-    public String getRecipes()
+    public String getRecipes(Model model)
     {
+        model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
 }
